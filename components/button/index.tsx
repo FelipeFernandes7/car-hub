e6b90ad8dd54783bbd3be90d44d98370c1cd944f
Felipe Fernandes <felipekurosaki123@gmail.com>
2023-09-06 17:22:05 -0300

    implements button component

--- components/button/index.tsx
+++ components/button/index.tsx
@@ -3,26 +3,42 @@ import { MouseEventHandler } from "react";
 import Image from "next/image";
 
 export interface ButtonProps {
-  title: string;
-  containerStyles?: string;
+  isDisabled?: boolean;
   btnType?: "button" | "submit";
+  containerStyles?: string;
+  textStyles?: string;
+  title: string;
+  rightIcon?: string;
   handleClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
 export function Button({
   title,
-  containerStyles,
   btnType,
+  rightIcon,
+  isDisabled,
+  textStyles,
+  containerStyles,
   handleClick,
 }: ButtonProps) {
   return (
     <button
-      disabled={false}
+      disabled={isDisabled}
       type={btnType || "button"}
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
     >
-      <span className={`flex-1`}>{title}</span>
+      <span className={`flex-1 ${textStyles}`}>{title}</span>
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image
+            src={rightIcon}
+            alt="arrow_left"
+            fill
+            className="object-contain"
+          />
+        </div>
+      )}
     </button>
   );
 }
