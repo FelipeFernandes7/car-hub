0f3b082f66b6bb29ffa1df772741916c19c696c0
Felipe Fernandes <felipekurosaki123@gmail.com>
2023-09-06 17:21:46 -0300

    add new exports

--- components/index.ts
+++ components/index.ts
@@ -5,5 +5,17 @@ import { Navbar } from "./navbar";
 import { SearchBar } from "./search";
 import { Filter } from "./filter";
 import { SearchManufacturer } from "./manufacturer";
+import { CarCard } from "./card";
+import { CarDetail } from "./detail";
 
-export { Hero, Button, Navbar, Footer, SearchBar, Filter, SearchManufacturer };
+export {
+  Hero,
+  Button,
+  Navbar,
+  Footer,
+  SearchBar,
+  Filter,
+  SearchManufacturer,
+  CarCard,
+  CarDetail,
+};
