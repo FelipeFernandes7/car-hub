7f664cfb5e69c464fde4671d61afca71a0d8a904
Felipe Fernandes <felipekurosaki123@gmail.com>
2023-09-06 13:53:24 -0300

    import components

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,8 @@ import { Button } from "./button";
 import { Hero } from "./hero";
 import { Footer } from "./footer";
 import { Navbar } from "./navbar";
+import { SearchBar } from "./search";
+import { Filter } from "./filter";
+import { SearchManufacturer } from "./manufacturer";
 
-export { Hero, Button, Navbar, Footer };
+export { Hero, Button, Navbar, Footer, SearchBar, Filter, SearchManufacturer };
